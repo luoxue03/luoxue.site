abbc4e022916837e04e53e135218a7395ce42087
Luoxue <1355440906@qq.com>
2023-11-07 15:32:20 +0800

    Build at 2023-11-2023年11月7日 15:32:16 +8

--- js/plugins/dist/counter.dev.js
+++ js/plugins/dist/counter.dev.js
@@ -1,19 +1,23 @@
+"use strict";
+
 !function () {
   var e = document.currentScript,
-    t = e.getAttribute("interval") || "2000",
-    o = e.getAttribute("room") || "",
-    n = e.getAttribute("api") || "http://localhost:8080/counter";
+      t = e.getAttribute("interval") || "2000",
+      o = e.getAttribute("room") || "",
+      n = e.getAttribute("api") || "http://localhost:8080/counter";
   localStorage.setItem("room", o);
 
-  var r = function () {
+  var r = function r() {
     var e = new XMLHttpRequest(),
-      l = n;
+        l = n;
+
     if ("" !== o) {
       l = "".concat(n, "?room=").concat(o);
     }
-    e.open("GET", l, !0);
 
+    e.open("GET", l, !0);
     var c = localStorage.getItem("token");
+
     if (null !== c) {
       e.setRequestHeader("Authorization", "Bearer " + c);
     }
@@ -21,12 +25,12 @@
     e.onload = function () {
       if (4 === e.readyState && 200 === e.status) {
         var n = JSON.parse(e.responseText);
+
         if (!0 === n.success) {
           var l = n.data;
           document.getElementById("online_user").innerHTML = l.online_user;
           document.getElementById("online_total").innerHTML = a(l.online_total);
           document.getElementById("online_me").innerHTML = a(l.online_me);
-
           var s = e.getResponseHeader("Set-Token");
 
           if (null == c && null != s && s.trim() !== "") {
@@ -50,11 +54,11 @@
     e.send();
   };
 
-  var a = function (e) {
+  var a = function a(e) {
     var t = Math.floor(e / 86400),
-      o = Math.floor(e % 86400 / 3600),
-      n = Math.floor(e % 3600 / 60),
-      r = Math.floor(e % 60);
+        o = Math.floor(e % 86400 / 3600),
+        n = Math.floor(e % 3600 / 60),
+        r = Math.floor(e % 60);
     return "".concat(t, "d ").concat(o, "h ").concat(n, "m ").concat(r, "s");
   };
 
